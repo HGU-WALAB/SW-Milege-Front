4def5eecbe6a5841c8030c67537a08867948ea6e
ohinhyuk <8156217@naver.com>
2023-08-17 12:47:37 +0900

    style : 버튼 템플릿 완료

---
+++ src/pages/template/index.js
@@ -0,0 +1,27 @@
+import { Box, Button } from '@mui/material';
+import FilledButton from 'src/components/Template/FilledButton';
+import OutlinedButton from 'src/components/Template/OutlinedButton';
+import TextButton from 'src/components/Template/TextButton';
+
+export default function Template() {
+  return (
+    <Box sx={{ width: '100vw', height: '100vh', p: '20px' }}>
+      <Box
+        sx={{
+          px: 5,
+          py: 5,
+          borderRadius: '10px',
+          border: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px',
+          width: '200px',
+        }}
+      >
+        <FilledButton text="FilledButton" px="20px" py="5px" />
+        <OutlinedButton variant="outlined" text="outlinedButton" px="20px" py="5px" />
+        <TextButton variant="text" text="textButton" px="20px" py="5px" />
+      </Box>
+    </Box>
+  );
+}
