20ed275dbd27fbd62e6cf4f070f07a6b7a83bf2d
ohinhyuk <8156217@naver.com>
2023-08-17 13:55:35 +0900

    style : 헤더 컴포넌트 완성

---
+++ src/pages/sw/index.js
@@ -0,0 +1,12 @@
+import Header from 'src/components/common/Header';
+
+// import Header from 'src/layouts/dashboard/header/Header';
+
+export default function index() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Header />
+    </>
+  );
+}
