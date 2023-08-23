8320c9735a3cefe157fb46ab9dd10042d7ceb6aa
ohinhyuk <8156217@naver.com>
2023-08-24 00:28:36 +0900

    Feat: mUI 모달 컴포넌트 생성(#7)

--- src/pages/sw/index.js
+++ src/pages/sw/index.js
@@ -1,4 +1,6 @@
+import { Dialog } from '@mui/material';
 import { Board } from 'src/assets/data/board/board';
+import CustomModal1 from 'src/components/Template/CustomModal';
 import MiniDrawer from 'src/components/common/Drawer/Drawer';
 import Header from 'src/components/common/Header';
 
@@ -8,8 +10,11 @@ export default function index() {
   return (
     <>
       {/* <Header /> */}
+
       <Header />
+
       <MiniDrawer />
+      <Dialog />
     </>
   );
 }
