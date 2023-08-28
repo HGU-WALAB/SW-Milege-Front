77c1eca3fb6f40bfeb1294999b090ed444b1c340
ohinhyuk <8156217@naver.com>
2023-08-28 22:36:37 +0900

    Feat: 마일리지 등록 READ 연결(#24)

--- src/pages/mileage/register/index.tsx
+++ src/pages/mileage/register/index.tsx
@@ -33,6 +33,181 @@ export enum MileageRegisterBoard {
   'ADD' = ADD,
 }
 
+// index + 1,
+//         record.semesterName,
+//         record.itemName,
+//         record.records.length,
+//         <SWModal type={EDITCATEGORY} beforeData={beforeData} />
+/**
+ * @kind [마일리지 등록]
+ * @brief 데이터 생성 함수
+ *
+ *  */
+function createData(
+  num: number,
+  semester: string,
+  item: string,
+  description: string,
+  registerNum: number,
+  file: string,
+  modifyiedDate: string,
+  add: string
+): Data {
+  return {
+    [MileageRegisterBoard.NUM]: num,
+    [MileageRegisterBoard.SEMESTER]: semester,
+    [MileageRegisterBoard.ITEM]: item,
+    [MileageRegisterBoard.DESCRIPTION]: description,
+    [MileageRegisterBoard.REGISTER_NUM]: registerNum,
+    [MileageRegisterBoard.FILE]: file,
+    [MileageRegisterBoard.MODIFYIED_DATE]: modifyiedDate,
+    [MileageRegisterBoard.ADD]: add,
+  };
+}
+
+/**
+ * @kind [마일리지 등록]
+ * @brief 테이블 헤더
+ */
+const headCells = [
+  {
+    id: [MileageRegisterBoard.NUM],
+    numeric: false,
+    disablePadding: true,
+    label: '번호',
+  },
+  {
+    id: [MileageRegisterBoard.SEMESTER],
+    numeric: true,
+    disablePadding: false,
+    label: '학기',
+  },
+  {
+    id: [MileageRegisterBoard.ITEM],
+    numeric: true,
+    disablePadding: false,
+    label: '항목명',
+  },
+  {
+    id: [MileageRegisterBoard.DESCRIPTION],
+    numeric: true,
+    disablePadding: false,
+    label: '내용',
+  },
+  {
+    id: [MileageRegisterBoard.REGISTER_NUM],
+    numeric: true,
+    disablePadding: false,
+    label: '등록수',
+  },
+  {
+    id: [MileageRegisterBoard.FILE],
+    numeric: true,
+    disablePadding: false,
+    label: '파일',
+  },
+  {
+    id: [MileageRegisterBoard.MODIFYIED_DATE],
+    numeric: true,
+    disablePadding: false,
+    label: '수정일',
+  },
+  {
+    id: [MileageRegisterBoard.ADD],
+    numeric: true,
+    disablePadding: false,
+    label: '추가',
+  },
+];
+
+/**
+ * @kind [마일리지 등록]
+ * @description 마일리지 항목 리스트
+ */
+
+const rows = [
+  createData(
+    1,
+    '2022-01',
+    '웹 서비스 캠프',
+    '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    2,
+    '2022-01',
+    'C언어 캠프',
+    'C언어의 기초에 대해 공부하고 C언어를 이용하여 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2022-01-23',
+    <AddIcon />
+  ),
+  createData(
+    3,
+    '2022-01',
+    '파이썬 캠프',
+    '파이썬 기초를 공부하고 파이썬을 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    4,
+    '2022-01',
+    'C++ 캠프',
+    'C++ 기초를 공부하고 C++을 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    5,
+    '2022-01',
+    '자바 캠프',
+    '자바 기초를 공부하고 자바를 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    6,
+    '2022-02',
+    '웹 서비스 캠프',
+    '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    7,
+    '2022-02',
+    '데이터 구조',
+    '데이터 구조에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    8,
+    '2022-02',
+    '데이터베이스',
+    '데이터베이스에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+];
+
 /**
  * @kind [마일리지 등록]
  * @breif 데이터 인터페이스
@@ -47,176 +222,234 @@ interface Data {
   [MileageRegisterBoard.ADD]: string;
 }
 
-export default function MileageRegister() {
-  /**
-   * @kind [마일리지 등록]
-   * @brief 데이터 생성 함수
-   *
-   *  */
-  function createData(
-    num: number,
-    semester: string,
-    item: string,
-    description: string,
-    registerNum: number,
-    file: string,
-    modifyiedDate: string,
-    add: string
-  ): Data {
-    return {
-      [MileageRegisterBoard.NUM]: num,
-      [MileageRegisterBoard.SEMESTER]: semester,
-      [MileageRegisterBoard.ITEM]: item,
-      [MileageRegisterBoard.DESCRIPTION]: description,
-      [MileageRegisterBoard.REGISTER_NUM]: registerNum,
-      [MileageRegisterBoard.FILE]: file,
-      [MileageRegisterBoard.MODIFYIED_DATE]: modifyiedDate,
-      [MileageRegisterBoard.ADD]: add,
-    };
-  }
+/**
+ * @kind [마일리지 등록]
+ * @brief 데이터 생성 함수
+ *
+ *  */
+function createData(
+  num: number,
+  semester: string,
+  item: string,
+  description: string,
+  registerNum: number,
+  file: string,
+  modifyiedDate: string,
+  add: string
+): Data {
+  return {
+    [MileageRegisterBoard.NUM]: num,
+    [MileageRegisterBoard.SEMESTER]: semester,
+    [MileageRegisterBoard.ITEM]: item,
+    [MileageRegisterBoard.DESCRIPTION]: description,
+    [MileageRegisterBoard.REGISTER_NUM]: registerNum,
+    [MileageRegisterBoard.FILE]: file,
+    [MileageRegisterBoard.MODIFYIED_DATE]: modifyiedDate,
+    [MileageRegisterBoard.ADD]: add,
+  };
+}
 
-  /**
-   * @kind [마일리지 등록]
-   * @brief 테이블 헤더
-   */
-  const headCells = [
-    {
-      id: [MileageRegisterBoard.NUM],
-      numeric: false,
-      disablePadding: true,
-      label: '번호',
-    },
-    {
-      id: [MileageRegisterBoard.SEMESTER],
-      numeric: true,
-      disablePadding: false,
-      label: '학기',
-    },
-    {
-      id: [MileageRegisterBoard.ITEM],
-      numeric: true,
-      disablePadding: false,
-      label: '항목명',
-    },
-    {
-      id: [MileageRegisterBoard.DESCRIPTION],
-      numeric: true,
-      disablePadding: false,
-      label: '내용',
-    },
-    {
-      id: [MileageRegisterBoard.REGISTER_NUM],
-      numeric: true,
-      disablePadding: false,
-      label: '등록수',
-    },
-    {
-      id: [MileageRegisterBoard.FILE],
-      numeric: true,
-      disablePadding: false,
-      label: '파일',
-    },
-    {
-      id: [MileageRegisterBoard.MODIFYIED_DATE],
-      numeric: true,
-      disablePadding: false,
-      label: '수정일',
-    },
-    {
-      id: [MileageRegisterBoard.ADD],
-      numeric: true,
-      disablePadding: false,
-      label: '추가',
-    },
-  ];
+/**
+ * @kind [마일리지 등록]
+ * @brief 테이블 헤더
+ */
+const headCells = [
+  {
+    id: [MileageRegisterBoard.NUM],
+    numeric: false,
+    disablePadding: true,
+    label: '번호',
+  },
+  {
+    id: [MileageRegisterBoard.SEMESTER],
+    numeric: true,
+    disablePadding: false,
+    label: '학기',
+  },
+  {
+    id: [MileageRegisterBoard.ITEM],
+    numeric: true,
+    disablePadding: false,
+    label: '항목명',
+  },
+  {
+    id: [MileageRegisterBoard.DESCRIPTION],
+    numeric: true,
+    disablePadding: false,
+    label: '내용',
+  },
+  {
+    id: [MileageRegisterBoard.REGISTER_NUM],
+    numeric: true,
+    disablePadding: false,
+    label: '등록수',
+  },
+  {
+    id: [MileageRegisterBoard.FILE],
+    numeric: true,
+    disablePadding: false,
+    label: '파일',
+  },
+  {
+    id: [MileageRegisterBoard.MODIFYIED_DATE],
+    numeric: true,
+    disablePadding: false,
+    label: '수정일',
+  },
+  {
+    id: [MileageRegisterBoard.ADD],
+    numeric: true,
+    disablePadding: false,
+    label: '추가',
+  },
+];
 
-  /**
-   * @kind [마일리지 등록]
-   * @description 마일리지 항목 리스트
-   */
+/**
+ * @kind [마일리지 등록]
+ * @description 마일리지 항목 리스트
+ */
 
-  const rows = [
-    createData(
-      1,
-      '2022-01',
-      '웹 서비스 캠프',
-      '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
-      23,
-      <AttachFileIcon />,
-      '2021-01-21',
-      <AddIcon />
-    ),
-    createData(
-      2,
-      '2022-01',
-      'C언어 캠프',
-      'C언어의 기초에 대해 공부하고 C언어를 이용하여 간단한 프로그램을 만들어보는 과정',
-      23,
-      <AttachFileIcon />,
-      '2022-01-23',
-      <AddIcon />
-    ),
-    createData(
-      3,
-      '2022-01',
-      '파이썬 캠프',
-      '파이썬 기초를 공부하고 파이썬을 이용하여 간단한 프로그램을 만들어보는 과정',
-      15,
-      <AttachFileIcon />,
-      '2022-01-31',
-      <AddIcon />
-    ),
-    createData(
-      4,
-      '2022-01',
-      'C++ 캠프',
-      'C++ 기초를 공부하고 C++을 이용하여 간단한 프로그램을 만들어보는 과정',
-      15,
-      <AttachFileIcon />,
-      '2022-01-31',
-      <AddIcon />
-    ),
-    createData(
-      5,
-      '2022-01',
-      '자바 캠프',
-      '자바 기초를 공부하고 자바를 이용하여 간단한 프로그램을 만들어보는 과정',
-      15,
-      <AttachFileIcon />,
-      '2022-01-31',
-      <AddIcon />
-    ),
-    createData(
-      6,
-      '2022-02',
-      '웹 서비스 캠프',
-      '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
-      23,
-      <AttachFileIcon />,
-      '2021-01-21',
-      <AddIcon />
-    ),
-    createData(
-      7,
-      '2022-02',
-      '데이터 구조',
-      '데이터 구조에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
-      23,
-      <AttachFileIcon />,
-      '2021-01-21',
-      <AddIcon />
-    ),
-    createData(
-      8,
-      '2022-02',
-      '데이터베이스',
-      '데이터베이스에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
-      23,
-      <AttachFileIcon />,
-      '2021-01-21',
-      <AddIcon />
-    ),
-  ];
+const rows = [
+  createData(
+    1,
+    '2022-01',
+    '웹 서비스 캠프',
+    '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    2,
+    '2022-01',
+    'C언어 캠프',
+    'C언어의 기초에 대해 공부하고 C언어를 이용하여 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2022-01-23',
+    <AddIcon />
+  ),
+  createData(
+    3,
+    '2022-01',
+    '파이썬 캠프',
+    '파이썬 기초를 공부하고 파이썬을 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    4,
+    '2022-01',
+    'C++ 캠프',
+    'C++ 기초를 공부하고 C++을 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    5,
+    '2022-01',
+    '자바 캠프',
+    '자바 기초를 공부하고 자바를 이용하여 간단한 프로그램을 만들어보는 과정',
+    15,
+    <AttachFileIcon />,
+    '2022-01-31',
+    <AddIcon />
+  ),
+  createData(
+    6,
+    '2022-02',
+    '웹 서비스 캠프',
+    '웹 서비스 구축에 필요한 스터디를 진행하고 직접 자신만의 웹페이지를 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    7,
+    '2022-02',
+    '데이터 구조',
+    '데이터 구조에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+  createData(
+    8,
+    '2022-02',
+    '데이터베이스',
+    '데이터베이스에 대해 공부하고 간단한 프로그램을 만들어보는 과정',
+    23,
+    <AttachFileIcon />,
+    '2021-01-21',
+    <AddIcon />
+  ),
+];
+interface Record {
+  id: number;
+  studentName: string;
+  counts: number;
+  points: number;
+  extraPoints: number;
+  description1: string;
+  description2: string;
+}
 
-  return <EnhancedTable originalRows={rows} headCells={headCells} type="마일리지 등록" />;
+interface semesterItemsWithStudents {
+  id: number;
+  itemName: string;
+  categoryName: string;
+  semesterName: string;
+  points: number;
+  itemMaxPoints: number;
+  categoryMaxPoints: number;
+  records: Record[];
+}
+
+type semesterItemsWithStudentList = semesterItemsWithStudents[];
+
+export const getServerSideProps: GetServerSideProps<{
+  fetchData: semesterItemsWithStudentList;
+}> = async () => {
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_API_KEY}/api/mileage/categories`);
+  const res = await axiosInstance.get('/api/mileage/semesters/2023-01/items/records');
+  const fetchData = res.data;
+  console.log(fetchData);
+  return { props: { fetchData } };
+};
+export default function MileageRegister({
+  fetchData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const convertedFetchList = fetchData.semesterItemsWithRecords?.map((record, index) => {
+    return fetchData.categories?.map((item) => {
+      const beforeData = {
+        semesterItemId: record.id,
+        studentId: record.records[index].id,
+        recordId: record.records[index].id,
+        counts: record.records[index].counts,
+        points: record.records[index].points,
+        extraPoints: record.records[index].extraPoints,
+        description1: record.records[index].description1,
+        description2: record.records[index].description2,
+      };
+
+      return createData(
+        index + 1,
+        record.semesterName,
+        record.itemName,
+        record.records.length,
+        <SWModal type={EDITCATEGORY} beforeData={beforeData} />
+      );
+    });
+  });
+
+  return (
+    <EnhancedTable originalRows={convertedFetchList} headCells={headCells} type="마일리지 등록" />
+  );
 }
