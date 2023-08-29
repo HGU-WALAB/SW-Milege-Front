1da7a95899c5bbe4805ee7a56b9b614d97775aeb
ohinhyuk <8156217@naver.com>
2023-08-29 23:50:42 +0900

    Style: 카테고리 필터링의 스타일 변경(#15)

--- src/components/common/Filter/CategoryAutoComplete.tsx
+++ src/components/common/Filter/CategoryAutoComplete.tsx
@@ -1,18 +1,23 @@
 import { Autocomplete, TextField } from '@mui/material';
+import { styled } from '@mui/system';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCategory } from 'src/redux/slices/filter';
 
-export default function CategoryAutoComplete() {
-  const top100Films = [
-    '전체',
-    '전공 마일리지',
-    '비교과 - 연구활동',
-    '비교과 - 특강참여',
-    '비교과 - 학회활동',
-    '비교과 - 행사참여',
-  ];
+const StyledAutocomplete = styled(Autocomplete)({
+  width: '200px',
+});
 
+const top100Films = [
+  '전체',
+  '전공 마일리지',
+  '비교과 - 연구활동',
+  '비교과 - 특강참여',
+  '비교과 - 학회활동',
+  '비교과 - 행사참여',
+];
+
+export default function CategoryAutoComplete() {
   const value = useSelector((state) => state.filter.category);
   const dispatch = useDispatch();
 
@@ -21,7 +26,7 @@ export default function CategoryAutoComplete() {
   };
 
   return (
-    <Autocomplete
+    <StyledAutocomplete
       size="small"
       value={value}
       disablePortal
