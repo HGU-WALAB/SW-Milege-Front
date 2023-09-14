f6815cdfcb0ed0eec585e7beac7171c24a3babce
ohinhyuk <8156217@naver.com>
2023-09-15 04:11:03 +0900

    Feat: drawer pinned가 참일 때는 drawer가 닫히지 않도록 설정(#34)

--- src/components/common/Drawer/Drawer.tsx
+++ src/components/common/Drawer/Drawer.tsx
@@ -108,7 +108,7 @@ export default function MiniDrawer() {
   };
 
   const handleDrawerClose = () => {
-    drawerClose();
+    if (!pinned) drawerClose();
   };
 
   const handleDrawerPinOn = () => {
