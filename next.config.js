bcf5d1f9426776d20d5b2046c34b4383ca01c926
ohinhyuk <8156217@naver.com>
2023-09-15 12:00:13 +0900

    Chore : next Config 설정을 통해 strictMode에서도 드래그 앤드랍 사용할 수 있도록 설정(#37)

--- next.config.js
+++ next.config.js
@@ -27,6 +27,13 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 
   reactStrictMode: true,
 
