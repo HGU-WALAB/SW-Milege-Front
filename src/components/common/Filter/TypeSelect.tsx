bbb014fd5c346fcd92f3a39b69baf14c897436bd
ohinhyuk <8156217@naver.com>
2023-10-06 03:02:09 +0900

    Feat: 카테고리 추가/수정 폼에 타입 Select를 만들어 추가시킴(#57)

---
+++ src/components/common/Filter/TypeSelect.tsx
@@ -0,0 +1,52 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { ErrorMessage, Field } from 'formik';
+import { useSelector } from 'react-redux';
+import { CATEGORYID, SEMESTER, TYPE } from 'src/assets/data/fields';
+
+export default function TypeSelect() {
+  const TYPES = [
+    {
+      id: 0,
+      text: 'A타입',
+      value: 'A',
+    },
+    {
+      id: 1,
+      text: 'B타입',
+      value: 'B',
+    },
+    {
+      id: 2,
+      text: 'C타입',
+      value: 'C',
+    },
+    {
+      id: 3,
+      text: 'D타입',
+      value: 'D',
+    },
+    {
+      id: 4,
+      text: 'E타입',
+      value: 'E',
+    },
+  ];
+
+  const MySelect = ({ field, form, ...props }) => (
+    <Select {...field} {...props}>
+      {TYPES.map((type, index) => (
+        <MenuItem key={type.id} value={type.value}>
+          {type.text}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+
+  return (
+    <FormControl sx={{ width: '100%' }}>
+      <InputLabel id="demo-simple-select-label">타입</InputLabel>
+      <Field as={MySelect} name={TYPE} />
+      <ErrorMessage name={TYPE} />
+    </FormControl>
+  );
+}
