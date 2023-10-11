36033500501447a4224b3f69eaefc618378f562e
ohinhyuk <8156217@naver.com>
2023-10-12 05:21:16 +0900

    Fix: Semester Select 컴포넌트 학기 하드 코딩 -> 현재 연도 기준 +- 4년 동안의 학기 동적 생성(#65)

--- src/components/common/Select/SemesterSelect.tsx
+++ src/components/common/Select/SemesterSelect.tsx
@@ -3,25 +3,42 @@ import { ErrorMessage, Field } from 'formik';
 import { useSelector } from 'react-redux';
 import { CATEGORYID, SEMESTER } from 'src/assets/data/fields';
 
+const generateSemesters = (currentYear) => {
+  const startYear = currentYear - 4;
+  const endYear = currentYear + 4;
+  const semesters = [];
+
+  for (let year = startYear; year <= endYear; year++) {
+    semesters.push(`${year}-01`);
+    semesters.push(`${year}-02`);
+  }
+
+  return semesters;
+};
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
 export default function SemesterSelect() {
-  const semesters = [
-    '2020-01',
-    '2020-02',
-    '2021-01',
-    '2021-02',
-    '2022-01',
-    '2022-02',
-    '2023-01',
-    '2023-02',
-    '2024-01',
-    '2024-02',
-    '2025-01',
-    '2025-02',
-    '2026-01',
-    '2026-02',
-    '2027-01',
-    '2027-02',
-  ];
+  // const semesters = [
+  //   '2020-01',
+  //   '2020-02',
+  //   '2021-01',
+  //   '2021-02',
+  //   '2022-01',
+  //   '2022-02',
+  //   '2023-01',
+  //   '2023-02',
+  //   '2024-01',
+  //   '2024-02',
+  //   '2025-01',
+  //   '2025-02',
+  //   '2026-01',
+  //   '2026-02',
+  //   '2027-01',
+  //   '2027-02',
+  // ];
+  const semesters = generateSemesters(currentYear);
 
   const MySelect = ({ field, form, ...props }) => (
     <Select {...field} {...props}>
