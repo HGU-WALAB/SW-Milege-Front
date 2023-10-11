383704ccbc5712178a62b8f20854e4ae20e60637
ohinhyuk <8156217@naver.com>
2023-10-12 05:53:09 +0900

    Feat : 엑셀 임포트 API 연결 및 자동화(#55)

--- src/components/excel/ExcelImport.tsx
+++ src/components/excel/ExcelImport.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { Button } from '@mui/material';
+import axiosInstance from 'src/utils/axios';
 
 export default function ExcelImport() {
   const [selectedFile, setSelectedFile] = useState();
   const inputRef = useRef(null); // useRef 추가
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    // e.preventDefault();
 
     const formData = new FormData();
 
@@ -15,10 +16,30 @@ export default function ExcelImport() {
 
     console.log(selectedFile);
     // console.log(importCourses(formData));
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+    // excelType
+    // 1) semesterIn
+    // 2) mileageRecord
+    axiosInstance.post(`/api/excel/upload/semesterIn`, formData, config).then(
+      (response) => {
+        alert('엑셀 업로드에 성공했습니다.');
+        console.log(response);
+      },
+      (error) => {
+        alert('엑셀 업로드에 실패했습니다.');
+        console.log(error);
+      }
+    );
   };
 
   const handleChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+    setSelectedFile(event.currentTarget.files[0]);
+    handleSubmit(event);
   };
 
   const handleButtonClick = () => {
