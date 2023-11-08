a0d6306b4b957563330af1d5354ac9a871cd5a23
ohinhyuk <8156217@naver.com>
2023-11-09 03:46:28 +0900

    Style: 학기 신청 기간 테이블 색상 변경(#84)

--- src/components/common/Table/SemesterTable.tsx
+++ src/components/common/Table/SemesterTable.tsx
@@ -31,11 +31,11 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
+    // backgroundColor: theme.palette.action.hover,
   },
   // hide last border
   '&:last-child td, &:last-child th': {
-    border: 0,
+    // border: 1,
   },
 }));
 
