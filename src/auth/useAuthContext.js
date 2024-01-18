fbd5663c4e3c05e886a55317edbb666548237685
Inhyuk Oh <49269218+ohinhyuk@users.noreply.github.com>
2024-01-19 02:21:58 +0900

    Fix: [관리자 관리 페이지] getServerSideProps 서버 단에서 에러 캐치 후 클라이언트 단에 에러 전달 과정에서 전달 에러 (#104)
    * Fix: 중복 Import 수정
    
    * Fix: 페이지 배포 라우팅 변경 및 루트 페이지 에러처리 로직 추가
    
    * Fix: 불필요한 authProvider 로직 제거
    
    * Fix: 로그인 , 로그아웃 경로 처리

--- src/auth/useAuthContext.js
+++ src/auth/useAuthContext.js
@@ -8,7 +8,7 @@ import { AuthContext } from './JwtContext';
 // ----------------------------------------------------------------------
 
 export const useAuthContext = () => {
-  const context = useContext(undefined);
+  const context = useContext(AuthContext);
 
   if (!context) throw new Error('useAuthContext context must be use inside AuthProvider');
 
