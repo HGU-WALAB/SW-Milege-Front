63d783c3f3c8215f115d807c891b59e505d7d546
ParkJiSung <gggg0195@gmail.com>
2024-04-06 18:36:39 +0900

    fix: 학기별 마일리지 세부 항목(수정할 때 학기, 카테고리 → label로 변경 (수정 불가하게))

--- src/components/common/Select/GlobalItemSelect.tsx
+++ src/components/common/Select/GlobalItemSelect.tsx
@@ -2,12 +2,14 @@ import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { ErrorMessage, Field } from 'formik';
 import { useSelector } from 'react-redux';
 import { CATEGORYID, NUM } from 'src/assets/data/fields';
+import { EDITITEM } from 'src/assets/data/modal/modals';
 
 export default function GlobalItemSelect({ itemId }: { itemId?: number }) {
   const globalItemList = useSelector((state) => state.filterList.itemList);
+  const modalType = useSelector((state) => state.modal.modalType);
 
   const MySelect = ({ field, form, ...props }) => (
-    <Select {...field} {...props} defaultValue={itemId}>
+    <Select {...field} {...props} defaultValue={itemId} disabled={modalType === EDITITEM ? 1 : 0}>
       {globalItemList.map((globalItem) => (
         <MenuItem key={globalItem.id} value={globalItem.id}>
           {globalItem.name}
--- src/components/common/Select/SemesterSelect.tsx
+++ src/components/common/Select/SemesterSelect.tsx
@@ -2,13 +2,14 @@ import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { Field } from 'formik';
 import { useSelector } from 'react-redux';
 import { SEMESTER } from 'src/assets/data/fields';
+import { EDITITEM } from 'src/assets/data/modal/modals';
 
 export default function SemesterSelect() {
   const semesters = useSelector((state) => state.filterList.semesterList);
   const modalType = useSelector((state) => state.modal.modalType);
 
   const MySelect = ({ field, form, ...props }) => (
-    <Select {...field} {...props} disabled={modalType === 'EDITITEM' ? 1 : 0}>
+    <Select {...field} {...props} disabled={modalType === EDITITEM ? 1 : 0}>
       {semesters.map((semester, index) => (
         <MenuItem key={index} value={semester}>
           {semester}
