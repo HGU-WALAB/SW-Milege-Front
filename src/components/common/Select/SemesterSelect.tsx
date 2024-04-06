edda12dc8ab9a086795f2e3ff67cf3cc248795c5
ParkJiSung <gggg0195@gmail.com>
2024-04-06 18:30:26 +0900

    fix: 학기별 마일리지 세부 항목(수정할 때 학기, 카테고리 → label로 변경 (수정 불가하게))

--- src/components/common/Select/SemesterSelect.tsx
+++ src/components/common/Select/SemesterSelect.tsx
@@ -1,14 +1,14 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { ErrorMessage, Field } from 'formik';
+import { Field } from 'formik';
 import { useSelector } from 'react-redux';
-import { CATEGORYID, SEMESTER } from 'src/assets/data/fields';
-import { generateSemesters, currentYear } from 'src/utils/semesterAutoGenerate';
+import { SEMESTER } from 'src/assets/data/fields';
 
 export default function SemesterSelect() {
   const semesters = useSelector((state) => state.filterList.semesterList);
+  const modalType = useSelector((state) => state.modal.modalType);
 
   const MySelect = ({ field, form, ...props }) => (
-    <Select {...field} {...props}>
+    <Select {...field} {...props} disabled={modalType === 'EDITITEM' ? 1 : 0}>
       {semesters.map((semester, index) => (
         <MenuItem key={index} value={semester}>
           {semester}
